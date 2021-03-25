a1d0d89f9790bb330519fb0825ffd39826cc4e5b
ziitania <tjhaweszii@gmail.com>
2021-03-25 09:21:57 -0700

    commit tree 23258ccd9961c486006ee52ce467b169fb33de5d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driv, nameMatch) {
+    return driv.filter( drivMatch => drivMatch.toUpperCase() === nameMatch.toUpperCase());
+  }
+  
+  function fuzzyMatch(driv, letters) {
+    return driv.filter( drivMatch => drivMatch.toUpperCase().indexOf(letters.toUpperCase()) === 0);
+  }
+  
+  function matchName(driv, nameMatch) {
+    return driv.filter( drivMatch => drivMatch.name === nameMatch);
+  }
